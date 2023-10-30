0bb816272ea5ee217881828fa6c1481bcd49f73b
jrmro <105397430+jrmro@users.noreply.github.com>
2023-10-30 14:34:48 -0400

    Update SymmetricEncryption.class.php

--- SymmetricEncryption.class.php
+++ SymmetricEncryption.class.php
@@ -14,33 +14,37 @@
 * $encryptor = new SymmetricEncryption($secret_key);
 *
 * $encrypted_data = $encryptor->encrypt("Hello, this is a test message.");
-* echo "Encrypted Data: $encrypted_data\n";
+* echo "Encrypted Data: {$encrypted_data}\n";
 *
 * $decrypted_data = $encryptor->decrypt($encrypted_data);
-* echo "Decrypted Data: $decrypted_data\n";
+* echo "Decrypted Data: {$decrypted_data}\n";
 *
 * @license    MIT License
 * @author     Joseph Romero
 * @version    1.0
 * ...
 */
-class SymmetricEncryption {
+class SymmetricEncryption 
+{
 
     private $method;
     private $secret_key;
 
-    public function __construct($secret_key, $method = 'AES-256-CBC') {
+    public function __construct($secret_key, $method = 'AES-256-CBC') 
+    {
         $this->secret_key = $secret_key;
         $this->method = $method;
     }
 
-    public function encrypt($data) {
+    public function encrypt($data) 
+    {
         $nonce = openssl_random_pseudo_bytes(openssl_cipher_iv_length($this->method));
         $encrypted = openssl_encrypt($data, $this->method, $this->secret_key, 0, $nonce);
         return base64_encode($nonce . $encrypted);
     }
 
-    public function decrypt($data) {
+    public function decrypt($data) 
+    {
         $encrypted = base64_decode($data);
         $nonce = substr($encrypted, 0, openssl_cipher_iv_length($this->method));
         $encrypted = substr($encrypted, openssl_cipher_iv_length($this->method));
